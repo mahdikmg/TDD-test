4a035e852768acf8f23fd77e2b3266f66971e587
mehdi <mahdi.k.m.g.6@gmail.com>
2021-11-01 21:17:08 +0330

    layout updated

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app v-resize="handleResize">
     <v-main>
-      <router-view />
+      <router-view :windowWidth="windowWidth" :windowHeight="windowHeight" />
     </v-main>
+
+    <v-overlay :opacity="0.5" :value="$loading.state.status" style="z-index: 9">
+      <v-progress-circular indeterminate size="64" color="primary">
+      </v-progress-circular>
+    </v-overlay>
+
+    <v-slide-x-reverse-transition
+      v-for="(notif, index) in $notif.state.notifs"
+      :key="index"
+    >
+      <v-snackbar
+        :color="notif.color"
+        left
+        bottom
+        :style="`bottom: ${index * 60}px;`"
+        :absolute="true"
+        v-if="notif.show"
+        :value="notif.show"
+        @input="() => $notif.remove(index)"
+      >
+        {{ notif.msg }}
+      </v-snackbar>
+    </v-slide-x-reverse-transition>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-
+  components: {},
   data: () => ({
-    //
+    windowWidth: 0,
+    windowHeight: 0,
   }),
+  methods: {
+    handleResize() {
+      this.windowWidth = window.innerWidth;
+      this.windowHeight = window.innerHeight;
+    },
+  },
 };
 </script>
